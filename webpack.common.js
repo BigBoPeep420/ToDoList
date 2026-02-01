7b6027bc06af5f89ea9d0dfb5f956b510ad6fa0e
BigBoPeep <236509553+BigBoPeep420@users.noreply.github.com>
2026-02-01 04:29:18 -0600

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(import.meta.dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    //Creates index.html and injects tags
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|webp)$/i,
+        use: "asset/resource",
+      },
+      {
+        test: /\.svg$/i,
+        use: "asset/source",
+      },
+    ],
+  },
+};
